40d74d7cf30969a84edb0103e75fc39975914d8a
farahsamsi <1827034farahsamsiprome@gmail.com>
2024-10-22 16:06:41 +0600

    added images in asset and styled the layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import './App.css'
 import Blogs from './Components/Blogs/Blogs'
+import Bookmarks from './Components/BookMarks/Bookmarks'
 import Header from './Components/Header/Header'
 
 function App() {
 
 
   return (
-    <>
+    <main>
       <Header></Header>
-      <Blogs></Blogs>
-    </>
+      <div className='md:flex'>
+        <Blogs></Blogs>
+        <Bookmarks></Bookmarks>
+      </div>
+    </main>
   )
 }
 
